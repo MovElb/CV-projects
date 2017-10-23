ee72a2f79613c8f457dcab3fa917d7012b0de5de
Movses <movses@MacBook-Pro-Movses.local>
2017-10-23 21:34:40 +0300

    First commit

---
+++ seam_carving/code/seam_carve.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+def calc_br(img):
+    return 0.299 * img[:, :, 0] + 0.587 * img[:, :, 1] + 0.114 * img[:, :, 2]
+
+
+def y_deriv(x, y, image):
+    w = image.shape[1]
+
+    if y == 0:
+        return image[x][1] - image[x][0]
+    elif y == w - 1:
+        return image[x][w - 1] - image[x][w - 2]
+    else:
+        return image[x][y + 1] - image[x][y - 1]
+
+
+def x_deriv(x, y, image):
+    h = image.shape[0]
+
+    if x == 0:
+        return image[1][y] - image[0][y]
+    elif x == h - 1:
+        return image[h - 1][y] - image[h - 2][y]
+    else:
+        return image[x + 1][y] - image[x - 1][y]
+
+
+def gradient(brightness_map):
+    h, w = brightness_map.shape
+
+    grad = np.zeros(brightness_map.shape)
+    for i in range(h):
+        for j in range(w):
+            dx = x_deriv(i, j, brightness_map)
+            dy = y_deriv(i, j, brightness_map)
+            grad[i][j] = np.sqrt(dx**2 + dy**2)
+    return grad
+
+
+def min_carve_arr(grad):
+    h, w = grad.shape
+
+    c_arr = np.copy(grad)
+    for i in range(1, h):
+        for j in range(w):
+            if j == 0:
+                c_arr[i][j] += min(c_arr[i - 1][j], c_arr[i - 1][j + 1])
+            elif j == w - 1:
+                c_arr[i][j] += min(c_arr[i - 1][j - 1], c_arr[i - 1][j])
+            else:
+                c_arr[i][j] += min(c_arr[i - 1][j - 1], c_arr[i - 1][j],
+                                   c_arr[i - 1][j + 1])
+    return c_arr
+
+
+def argmin(array):
+    array = list(array)
+    return array.index(min(array))
+
+
+def carve_mask_and_coord(grad):
+    h, w = grad.shape
+    c_arr = min_carve_arr(grad)
+
+    last_row_coord = (h - 1, argmin(c_arr[-1]))
+    carve_coord = [last_row_coord]
+
+    carv_mask = np.zeros(grad.shape, dtype=np.int)
+    carv_mask[h - 1][last_row_coord[1]] = 1
+
+    for i in range(h - 1):
+        x, y = carve_coord[0]
+
+        if y == 0:
+            min_neighb = argmin(c_arr[x - 1][: 2])
+        elif y == w - 1:
+            min_neighb = argmin(c_arr[x - 1][w - 2:])
+        else:
+            min_neighb = argmin(c_arr[x - 1][y - 1: y + 2])
+
+        if y != 0:
+            if min_neighb == 0:
+                min_neighb = y - 1
+            elif min_neighb == 1:
+                min_neighb = y
+            else:
+                min_neighb = y + 1
+
+        carve_coord = [(x - 1, min_neighb)] + carve_coord
+        carv_mask[x - 1][min_neighb] = 1
+
+    return carv_mask, carve_coord
+
+
+def gradient_upper_bound(img):
+    return img.shape[0] * img.shape[1] * 256
+
+
+def apply_mask(energy_map, mask=None):
+    if mask is None:
+        return energy_map
+    e_up_b = gradient_upper_bound(energy_map)
+    return energy_map + e_up_b * mask
+
+
+def remove_carve(img, coord):
+    return np.asarray(list(map(lambda row, crd: np.delete(row, crd[1], 0), img, coord)))
+
+
+def avrg_px(img, coord):
+    x, y = coord
+
+    sum = img[x][y].astype(int)
+    num = 1
+    if y + 1 != img.shape[1]:
+        sum += img[x][y + 1].astype(int)
+        num += 1
+    if y != 0:
+        sum += img[x][y - 1].astype(int)
+        num += 1
+    return sum // num
+
+
+def add_carve(img, coord):
+    return np.asarray(list(map(lambda row, crd:
+                               np.insert(row, crd[1] + 1, avrg_px(img, crd), 0),
+                               img, coord)))
+
+
+def seam_carve(img, mode, mask=None):
+    if mode[: 4] == 'vert':
+        img = np.swapaxes(img, 0, 1)
+        if mask is not None:
+            mask = np.swapaxes(mask, 0, 1)
+
+    energy_img = calc_br(img)
+
+    carve_mask, coord = carve_mask_and_coord(apply_mask(gradient(energy_img), mask))
+
+    resized_mask = None
+    if mode[-6:] == "shrink":
+        if mask is not None:
+            resized_mask = remove_carve(mask, coord)
+        resized_img = remove_carve(img, coord)
+    else:
+        if mask is not None:
+            resized_mask = add_carve(mask, coord)
+        resized_img = add_carve(img, coord)
+
+    if mode[: 4] == 'vert':
+        carve_mask = np.swapaxes(carve_mask, 0, 1)
+        resized_img = np.swapaxes(resized_img, 0, 1)
+        if mask is not None:
+            resized_mask = np.swapaxes(resized_mask, 0, 1)
+
+    return resized_img, resized_mask, carve_mask
---
+++ sign_classification/code/fit_and_classify.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+from skimage.color import rgb2gray
+from skimage.transform import resize
+
+from sklearn.svm import SVC as svm_cl
+
+import warnings
+# warnings.simplefilter('ignore')
+
+
+def preprocess_img(img, size=(64, 64), preserve_rgb=False):
+    if not preserve_rgb:
+        img = rgb2gray(img)
+
+    img = resize(img, size)
+    return img
+
+
+def hog_histograms(magnitude, angles, cellx, celly, n_cellx, n_celly,
+                   orientations, orientation_histogram):
+    angle_period = 180. / orientations
+    for cy_cnt in range(n_celly):
+        for cx_cnt in range(n_cellx):
+            pnt_x = cellx * cx_cnt
+            pnt_y = celly * cy_cnt
+
+            histogram = np.zeros(orientations)
+            for y in range(pnt_y, pnt_y + celly):
+                for x in range(pnt_x, pnt_x + cellx):
+                    _angle = angles[y][x]
+                    bin_n = int(_angle // angle_period)
+                    relative_angle = _angle % angle_period
+                    partition = relative_angle / angle_period
+
+                    histogram[bin_n % orientations] += (1. - partition) * magnitude[y][x]
+                    histogram[(bin_n + 1) % orientations] += partition * magnitude[y][x]
+
+            orientation_histogram[cy_cnt][cx_cnt] = histogram
+
+    return orientation_histogram
+
+
+def extract_hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2),
+                norm_eps=1e-12, apply_sqrt=True):
+    img = preprocess_img(img)
+
+    if apply_sqrt:
+        img = np.sqrt(img)
+
+    dy, dx = np.gradient(img, .5)
+
+    magn = np.hypot(dx, dy)
+    angl = np.rad2deg(np.arctan2(dy, dx)) % 180
+
+    sz_y, sz_x = img.shape
+    cellx, celly = pixels_per_cell
+    blockx, blocky = cells_per_block
+
+    n_cellx = int(sz_x // cellx)
+    n_celly = int(sz_y // celly)
+
+    orientation_histogram = np.zeros((n_celly, n_cellx, orientations))
+    orientation_histogram = hog_histograms(magn, angl, cellx, celly, n_cellx, n_celly,
+                                           orientations, orientation_histogram)
+
+    n_blockx = n_cellx - blockx + 1
+    n_blocky = n_celly - blocky + 1
+
+    norm_blocks = np.zeros((n_blocky, n_blockx, blocky, blockx, orientations))
+
+    for y in range(n_blocky):
+        for x in range(n_blockx):
+            block = orientation_histogram[y: y + blocky, x: x + blockx, :]
+            norm_blocks[y, x, :] = block / np.sqrt(np.sum(block ** 2) + norm_eps ** 2)
+
+    return np.ravel(norm_blocks)
+
+
+def fit_and_classify(train_features, train_labels, test_features):
+    model = svm_cl(C=160)
+    model.fit(train_features, train_labels)
+    prdct = model.predict(test_features)
+    return prdct
